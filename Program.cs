25e219db87d60ecff2e7f6fcf5214e6f5e4c01a9
agos4567 <agosgalfre23@gmail.com>
2023-06-22 00:11:28 -0300

    falta 2 ultimas consignas

--- Program.cs
+++ Program.cs
@@ -3,116 +3,143 @@ Console.WriteLine("Hello, World!");
 
 
 
-int opcion;
-// bool anda= false;
-bool anda2=false;
+string? cadena;
+
+Console.WriteLine("Ingrese una cadena");
+
+cadena=Console.ReadLine();
+
+Console.WriteLine("La cadena ingresada es:" + cadena);
+
+int largo= cadena.Length;
+
+Console.WriteLine("La longitud de la cadena es:" + largo);
+
+string? cadena2 , cadena3;
+Console.WriteLine("Ingrese una segunda cadena");
+
+cadena2=Console.ReadLine();
+
+cadena= cadena + cadena2;
+
+Console.WriteLine("cadenas concatenadas:" + cadena);
+
+
+
+foreach (char c  in cadena)
+{
+   Console.WriteLine("Mostrado");
+  Console.WriteLine(c);
+}
+
+
+
+string? buscado;
+
+Console.WriteLine("Ingrese la palabra a buscar: ");
+buscado=Console.ReadLine();
+
+
+if (cadena.Contains(buscado))
+{
+  Console.WriteLine("La palabra encontrada es:" + cadena);
+}else
+{
+   Console.WriteLine("NO encontro la palabra: ");
+}
+
+ Console.WriteLine("");
+string? may , min;
+int contMin=0;
+int contMay=0;
 
 
 
 
-float a, b;
+
+
+
+for(int contador =0; contador< cadena.Length; contador++){
+if (char.IsLower(cadena, contador))
+{  
+  
+  contMin++;
+}
+
+if (char.IsUpper(cadena, contador))
+{  
+  
+  contMay++;
+}
+
+
+
+}
+
+if (contMin>0)
+{
+  may=cadena.ToUpper();
+   Console.WriteLine("La palabra convertida a mayusculas es:" + may);
+} if (contMay>0)
+{
+   min=cadena.ToLower();
+   Console.WriteLine("La palabra convertida a minisculas es:" + min);
+}
+
+
+
+
+
+
+
+
+
+
+
+/***********************************************************************/
+
+
+double x;
 int  mcm, mcd, a2, b2, minimo;
 int num1, num2;
 
 
+bool anda =false;
+
+int a1, b1;
 
 
-float x;
+  
 
-
-
-
-do
-{
-    
-
-    Console.WriteLine("Ingrese un numero: ");
-    // anda = float.TryParse(Console.ReadLine(), out a);// convierte lo que escribo en float
-
-   a = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero?
-
-
-
-
-
-      Console.WriteLine("Ingrese el segundo numero: ");
-
-
-      b = Convert.ToInt32(Console.ReadLine());
-
-
-
-    Console.WriteLine("Elija una opcion: ");
-
-        Console.WriteLine("1/ -Sumar-  2/ -Restar  3/-Multiplicar ");
-
-     
-     opcion=Convert.ToInt32(Console.ReadLine());
-
-           switch (opcion)
-    {
-        case 1: 
-            Console.WriteLine(a+b);
-            break;
-        case 2: 
-            Console.WriteLine(a-b);
-            break;
-        case 3: 
-            Console.WriteLine(a*b);
-            break;
-        case 4: 
-            if(b == 0){
-                Console.WriteLine("No es posible dividir en 0.");
-            }else{
-                Console.WriteLine(a/b);
-            }
-            break;
-        default:
-
-        Console.WriteLine("Opcion invalida.");
-        Console.ReadLine();
-        break;
-
-      
-    }
-
-
-
-  Console.WriteLine("Desea realizar otra operacion (1.SI / 0.NO):");
-    opcion = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero lo guarda en opcion para analiza si sale del do while o sigue preguntando?
-
-
-
-} while (opcion !=0);
-
-
-  Console.WriteLine("- - - - CALCULADORA2- - - -");
-
-  while (!anda2)
+  while (!anda)
   {
     
     Console.WriteLine("Ingrese un numero: ");
-    anda2 = float.TryParse(Console.ReadLine(), out x);// convierte lo que escribo en float
+    anda = double.TryParse(Console.ReadLine(), out x);// convierte lo que escribo en float
     
 
    
 
-        Console.WriteLine("Valor absoluto: "+ Math.Abs(x));
-        Console.WriteLine("Cuadrado: "+ Math.Pow(x, 2));
-        Console.WriteLine("Raiz Cuadrada: "+ Math.Sqrt(x));
-        Console.WriteLine("Seno: "+ Math.Sin(x));
-        Console.WriteLine("Coseno: "+ Math.Cos(x));
+        Console.WriteLine($"Valor absoluto de {x} :  {Math.Abs(x).ToString()}");
+        Console.WriteLine($"Cuadrado de {x} : {Math.Pow(x, 2).ToString()}");
+        Console.WriteLine($"Raiz Cuadrada de {x}:  {Math.Sqrt(x).ToString()}");
+        Console.WriteLine($"Seno de {x}:  {Math.Sin(x).ToString()}");
+        Console.WriteLine($"Coseno de {x}:  {Math.Cos(x).ToString()}");
         string astr = x.ToString();
         string[] textSplit = astr.Split(","); // le saca la coma
         Console.WriteLine("Parte entera: "+ textSplit[0]);
+        Console.WriteLine($"Parte entera de {x}: + {Math.Round(x).ToString()}");
       Console.WriteLine("Ingrese dos numeros: ");
 
 
          Console.WriteLine("Primer numero: ");
-    int a1 = Convert.ToInt32(Console.ReadLine());
-     Console.WriteLine("Segundo numero: ");
-   int  b1 = Convert.ToInt32(Console.ReadLine());
 
+
+anda=int.TryParse(Console.ReadLine(), out a1);
+
+     Console.WriteLine("Segundo numero: ");
+
+anda =int.TryParse(Console.ReadLine(), out b1);
    
     
    mcd=0;
@@ -161,19 +188,8 @@ do
 
   Console.ReadKey(true);
 
+        
  } 
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
